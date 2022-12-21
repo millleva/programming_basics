e13ee3ee3371ea4b0f4517318adef933371a9bbf
millleva <kochevamilenazp@gmail.com>
2022-12-21 20:47:10 +0200

    split project into 2 files and data folder

---
+++ RGZ_2/task_a/cipher.cpp
@@ -0,0 +1,11 @@
+#include "cipher.h"
+
+string toBinary(int n){
+    string r;
+    while(n != 0){
+        r = (n % 2 == 0 ? "0" : "1") + r; 
+        n /= 2;
+    }
+
+    return r;
+}
---
+++ RGZ_2/task_a/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "cipher.h" 
+
+using namespace std;
+
+
+
+int main()
+{
+    cout << toBinary(43) << endl;
+
+    return 0;
+}
