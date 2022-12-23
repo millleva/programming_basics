35c7641245bb2eca80bd38f8393ce589cb2ce434
millleva <kochevamilenazp@gmail.com>
2022-12-23 23:02:05 +0200

    start cli args validation

--- RGZ_2/task_a/main.cpp
+++ RGZ_2/task_a/main.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 #include "cipher.h" 
 
 using namespace std;
 
-int main()
+#define ARGS_NUM 4
+#define USAGE_MSG ":USAGE: exe [encipher|decipher] input bits output"
+
+bool cliArgsValid(char** argv){
+    string mode = argv[1];
+    
+    if(mode != "encipher" && mode != "decipher"){
+        cout << "Mode can be encipher or decipher" << endl;
+        return false;
+    }
+
+    return false;
+}
+
+int main(int argc, char** argv)
 {
+    // if(argc != ARGS_NUM || !cliArgsValid(argv)){
+    //     cout << USAGE_MSG << endl;
+    //     return 1;
+    // }
+
+    cliArgsValid(argv);
+
     // encipherFileToOtherFile(
     //     "data/numbers_input.txt",
     //     "data/bits_exchange.txt",
     //     "data/numbers_output.txt"
     // );
 
-    decipherFileToOtherFile(
-        "data/numbers_output.txt",
-        "data/bits_exchange.txt",
-        "data/numbers_deciphered.txt"
-    );
+    // decipherFileToOtherFile(
+    //     "data/numbers_output.txt",
+    //     "data/bits_exchange.txt",
+    //     "data/numbers_deciphered.txt"
+    // );
 
     return 0;
 }
